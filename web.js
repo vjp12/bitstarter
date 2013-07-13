9692d69452e6884d7d16d6f0299c61ba702af058
Ubuntu <ubuntu@ip-172-31-13-192.us-west-2.compute.internal>
2013-07-13 18:55:07 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+var buf = require('buf');
 
 app.get('/', function(request, response) {
     fs.writeFile('index.html');
